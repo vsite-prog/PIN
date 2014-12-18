79825bccc3403769cdf1413b18ff5fabaaef805f
VSITE Programiranje <ivan.britvic@vsite.hr>
2014-12-18 19:39:29 +0100

    Predavanje 9 - ADO.NET
    ADO.NET

---
+++ Predavanje9/Default.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lb_greska" runat="server" Text="" ForeColor="Red"></asp:Label><br /><br />
+        <asp:LinkButton ID="LinkButton1" runat="server" OnClick="LinkButton1_Click">Pokaži životinje</asp:LinkButton>
+        <div id="tableDiv" runat="server">
+    
+        </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Predavanje9/Default.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data.SqlServerCe;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void LinkButton1_Click(object sender, EventArgs e)
+    {
+        string html = "<table><thead><tr><th>ID</th><th>Naziv</th><th>Vrsta</th></tr></thead><tbody>";
+        //Pročitaj iz web.config-a
+        string connString = WebConfigurationManager
+            .ConnectionStrings["ZivotinjeConnectionString"]
+            .ConnectionString;
+        //Nova konekcija - objekt
+        SqlCeConnection conn = new SqlCeConnection();
+        //podaci za konekciju 
+        conn.ConnectionString = connString;
+        //SAd treba SQL, prvo command objekt
+        SqlCeCommand command = new SqlCeCommand();
+        //Tekst SQL naredbe
+        command.CommandText = "SELECT * FROM zivotinje";
+        //Mi dajemo SQL
+        command.CommandType = System.Data.CommandType.Text;
+        //Evo i konekcije, zaboravili...
+        command.Connection = conn;
+        try
+        {
+            //POKUŠAJ OTVORITI
+            conn.Open();
+            //treba izvršiti komandu i prihvatiti podatke
+            SqlCeDataReader reader = command.ExecuteReader();
+            //Čitaj red po red
+            while (reader.Read())
+            {
+                //upiši novi red u HTML tablicu
+                html += "<tr><td>" + reader["id"].ToString() + "</td><td>" 
+                    + reader["naziv"] + "</td><td>" 
+                    + reader["vrsta"] + "</td></tr>";
+            }
+
+        }
+        catch (Exception ex)
+        {
+            lb_greska.Text = "Greška: " + ex.Message;
+        }
+        finally
+        {
+            //obvezno zatvoriti
+            conn.Close();
+        }
+
+        
+        html += "</tbody></table>";
+        tableDiv.InnerHtml = html;
+    }
+}
