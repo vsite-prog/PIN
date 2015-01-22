0bd5a9058aa92d61fc0373a76a4afb434ea95a59
VSITE Programiranje <ivan.britvic@vsite.hr>
2015-01-22 16:24:12 +0100

    Predavanje 13 - Entity Framework
    Entity Framework i LINQ

---
+++ Predavanje13/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+      
+         <asp:Label ID="Label2" runat="server" Text="Država: "></asp:Label>
+        <asp:DropDownList ID="ddl_drzava" runat="server" OnSelectedIndexChanged="ddl_drzava_SelectedIndexChanged" AutoPostBack="true"></asp:DropDownList>
+        <br />  <h4> Gradovi</h4><br />
+        <asp:GridView ID="gv_gradovi" runat="server"></asp:GridView>
+        <h4> Novi grad</h4>
+        <asp:Label ID="Label1" runat="server" Text="Grad: "></asp:Label>
+        <asp:TextBox ID="tb_grad" runat="server"></asp:TextBox>
+        <br />
+       <br />
+        <asp:Button ID="Button1" runat="server" Text="Spremi" OnClick="Button1_Click" />
+    <div>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Predavanje13/Default.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    //Handle na bazu i kontekst
+    DatabaseEntities db;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        db = new DatabaseEntities();
+
+        if (!IsPostBack)
+        {
+            //Kaži što je štu DDL
+            ddl_drzava.DataValueField = "id";
+            ddl_drzava.DataTextField = "naziv";
+            //Poveži se sa tablicom države
+            ddl_drzava.DataSource = db.drzave.ToList<drzave>();
+            ddl_drzava.DataBind();
+
+            pokaziGradove();
+
+        }
+
+
+
+        /*Lista gradova
+        gv_gradovi.DataSource = db.gradovi.ToList<gradovi>();
+        gv_gradovi.DataBind();
+         * */
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        //Spremi novi grad
+        gradovi grad = new gradovi();
+        grad.naziv = tb_grad.Text;
+        grad.drzavaId = Int32.Parse(ddl_drzava.SelectedValue);
+        //dodaj novi grad u listu
+        db.gradovi.Add(grad);
+        //Sve spremi u bazu
+        db.SaveChanges();
+
+        //Sve pokaži
+        pokaziGradove();
+    }
+
+    private void pokaziGradove()
+    {
+        // Pročitaj državu
+        int drzavaId = Int32.Parse(ddl_drzava.SelectedValue);
+        //Idi kroz sve gradove i vrati one iz naše države
+        var sviGradovi = from g in db.gradovi
+                              where g.drzavaId == drzavaId
+                              select new
+                              {
+                                  ID = g.id,
+                                  Naziv = g.naziv
+                              };
+        //POveži sa gridom
+        gv_gradovi.DataSource = sviGradovi.ToList(); //Prebaci iz DBSet u listu
+        gv_gradovi.DataBind();
+    }
+    protected void ddl_drzava_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        //Sve pokaži
+        pokaziGradove();
+    }
+}
