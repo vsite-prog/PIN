3e2e7dd5a6d8e8a41474d33e57876f66efb76b6e
VSITE Programiranje <ivan.britvic@vsite.hr>
2015-11-05 19:07:44 +0100

    Predavanje 5
    ASP.NET Events and ViewState

---
+++ Predavanje 5/Default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lb_otvaranje" runat="server" Text=""></asp:Label>
+        <p>
+            <asp:Literal ID="lt_txt" runat="server"></asp:Literal>
+            <asp:Label ID="lb_text" runat="server" Text="Haj"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="lb_poruka" runat="server" Text="Label"></asp:Label>
+        </p>
+        <asp:TextBox ID="tb_a" runat="server"></asp:TextBox> &nbsp;&nbsp;
+        <asp:DropDownList ID="DropDownList1"  AutoPostBack="true" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+            <asp:ListItem>+</asp:ListItem>
+            <asp:ListItem>-</asp:ListItem>
+            <asp:ListItem>*</asp:ListItem>
+            <asp:ListItem>/</asp:ListItem>
+        </asp:DropDownList>&nbsp;&nbsp;
+        <asp:TextBox ID="tb_b" runat="server"></asp:TextBox>&nbsp;&nbsp;
+        <asp:Label ID="lb_rez" runat="server"></asp:Label>
+        <br /><br />
+        <asp:LinkButton ID="lb_racunaj" runat="server" OnClick="lb_racunaj_Click">Računaj</asp:LinkButton>
+        <br />
+    
+        <asp:LinkButton ID="lB_pritisni" runat="server" OnClick="lB_pritisni_Click">Pritisni me</asp:LinkButton>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Predavanje 5/Default.aspx.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    int i = 0;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        int brojac; //This will work
+        i++; //Doesn't work - always 1
+        lb_text.Text += Server.HtmlEncode("<Page Load>"); //Escape HTML characters
+        if (!Page.IsPostBack) //Samo prvi put
+        {
+            lb_text.ForeColor = System.Drawing.Color.BlueViolet;
+            lt_txt.Text += "Prvo otvaranje <br>";
+            ViewState["brojac"] = 1;
+        }
+        else
+        {
+            brojac = (int)ViewState["brojac"];
+            brojac++; //increment counter
+            // lt_txt.Text += "Otvaranje br: " + i.ToString() + " <br> ";
+            lt_txt.Text += "Otvaranje br: " + brojac.ToString() + " <br> ";
+            ViewState["brojac"] = brojac;
+
+        }
+    }
+
+    protected void lB_pritisni_Click(object sender, EventArgs e)
+    {
+        lt_txt.Text += Server.HtmlEncode("Pritsnuli ste me!  <br>");
+        lb_text.Text += Server.HtmlEncode("<Button Click>");
+
+    }
+
+    private void InitializeComponent()
+    {
+            this.Init += new System.EventHandler(this._Default_Init);
+
+    }
+
+    private void _Default_Init(object sender, EventArgs e)
+    {
+        //  lb_text.Text += "<Page Init>";
+        lb_text.Text += Server.HtmlEncode("<Page Init>"); 
+    }
+
+    protected void lb_racunaj_Click(object sender, EventArgs e)
+    {
+       // calculate();
+    }
+
+    private void handleError(string s)
+    {
+        lb_poruka.Text = s;
+        lb_poruka.ForeColor = System.Drawing.Color.Red;
+
+    }
+
+    private void calculate()
+    {
+        //Calculator logic
+        double a, b, rez = 0;
+        //a = Double.Parse(tb_a.Text); throws Exception
+        if (!Double.TryParse(tb_a.Text, out a)) //Not a number
+        {
+            handleError("1. number is wrong");
+            return;
+        }
+        if (!Double.TryParse(tb_b.Text, out b)) //Not a number
+        {
+            handleError("2. number is wrong");
+            return;
+        }
+
+        switch (DropDownList1.SelectedValue)
+        {
+            case "+":
+                rez = a + b;
+                break;
+            case "-":
+                rez = a - b;
+                break;
+            case "*":
+                rez = a * b;
+                break;
+            case "/":
+                if (b == 0)
+                {
+                    handleError("Divide by zero");
+                    return;
+                }
+                rez = a / b;
+                break;
+            default:
+                rez = 0; //No need for this
+                break;
+        }
+        lb_rez.Text = rez.ToString();
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        calculate();
+    }
+}
