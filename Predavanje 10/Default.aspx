5e19eee15cc35a5840dd76e3a7e4fc9a82e4ba04
VSITE Programiranje <ivan.britvic@vsite.hr>
2015-12-16 17:40:49 +0100

    Predavanje 10
    ADO.NET

---
+++ Predavanje 10/Default.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Label ID="lb_poruka" runat="server" Text="" ForeColor="Red"></asp:Label>
+        <asp:Button ID="bt_mostovi" OnClick="bt_mostovi_Click" runat="server" Text="Učitaj" />
+    <h4> Mostovi</h4>
+        <div id="tableDiv" runat="server"></div>
+    </div>
+        <br /><br />
+        <h4>Unesi most</h4>
+        <asp:Label ID="Label1" runat="server" Text="Naziv: " ></asp:Label>
+        <asp:TextBox ID="tb_naziv" runat="server"></asp:TextBox><br />
+        <asp:Label ID="Label2" runat="server" Text="Mjesto: " ></asp:Label>
+        <asp:TextBox ID="tb_mjesto" runat="server"></asp:TextBox><br />
+        <asp:Button ID="Button1" OnClick="Button1_Click" runat="server" Text="Unesi" />
+    </form>
+</body>
+</html>
---
+++ Predavanje 10/Default.aspx.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Configuration;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void bt_mostovi_Click(object sender, EventArgs e)
+    {
+        //Read data from web.config needed for db connect
+        string connStr = WebConfigurationManager.ConnectionStrings["BazaCS"].ConnectionString;
+        SqlConnection conn = new SqlConnection(connStr); //Create connection object
+        SqlCommand comm = new SqlCommand("SELECT * FROM Most"); //Fetch all rows from most table
+        comm.Connection = conn; //set reference to connection object
+        comm.CommandType = System.Data.CommandType.Text; //just send text to db
+        try
+        {
+            //Open a connection to db
+            conn.Open();
+            SqlDataReader reader = comm.ExecuteReader(); //Fetch data from db and create new DR object
+            if (reader.HasRows)
+            {
+                string tabla = "<table><tr><th>ID</th><th>Naziv</th><th>Mjesto</th></tr>";
+                while (reader.Read()) //go to next record
+                {
+                    tabla += "<tr><td>" + reader[0] + "</td><td>" + reader[1] + "</td><td>" + reader[2] + "</td></tr>"; //Must be a number, order is important!
+                }
+                tabla += "</table>";
+                tableDiv.InnerHtml = tabla; //inject  HTML
+            } else
+            {
+                lb_poruka.Text = "Nema nikakvih mostova...";
+            }
+            
+        } catch (Exception ex)        {
+            lb_poruka.Text = "Greška: " + ex.Message;
+        } finally
+        {
+            //always close db connection
+            conn.Close();
+        }
+         
+    }
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        //Read data from web.config needed for db connect
+        string connStr = WebConfigurationManager.ConnectionStrings["BazaCS"].ConnectionString;
+        SqlConnection conn = new SqlConnection(connStr); //Create connection object
+        SqlCommand comm = new SqlCommand("INSERT INTO Most VALUES(@mjesto, @naziv)"); //Insert a new record in db
+        comm.Connection = conn; //set reference to connection object
+        comm.CommandType = System.Data.CommandType.Text; //just send text to db
+        comm.Parameters.AddWithValue("mjesto", tb_mjesto.Text); // put a user input in param
+        comm.Parameters.AddWithValue("naziv", tb_naziv.Text);
+        try
+        {
+            //Open a connection to db
+            conn.Open();
+            int brojRedova = comm.ExecuteNonQuery(); //Receive nr of affected rows
+            if (brojRedova == 1){
+                lb_poruka.Text = "Most unesen!";
+            } else
+            {
+                lb_poruka.Text = "Neispravan unos!";
+            }
+        } catch (Exception ex) {    
+            lb_poruka.Text = "Greška: " + ex.Message;
+        } finally
+        {
+            //always close db connection
+            conn.Close();
+        }
+    }
+}
