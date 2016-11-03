96c299b16569952dc495888917e81cad2ac6a9a0
vsite-prog <ivan.britvic@vsite.hr>
2016-11-03 18:07:23 +0100

    Osnovne web kontrole
    Predavanje 4 - Osnovne web kontrole

---
+++ Predavanje4/Default.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs"
+Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+   <title>Predavanje 4</title>
+  </head>
+<body>
+   <form id="form1" runat="server">
+         <div>
+       <p id="p1" runat="server"> Početni tekst</p>
+       <asp:Label ID="Label1" runat="server" Text="Unesi broj 1: "
+AssociatedControlID="tb_broj1"></asp:Label><asp:TextBox ID="tb_broj1"
+runat="server"></asp:TextBox>
+       <br />
+       <br />
+       <asp:Label ID="lb2" runat="server" Text="Unesi broj 2: "
+/><asp:TextBox ID="tb_broj2" runat="server"></asp:TextBox>
+       <br /><br />
+               <asp:DropDownList ID="DropDownList1" runat="server"
+OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged"
+AutoPostBack="true">
+                   <asp:ListItem>+</asp:ListItem>
+                   <asp:ListItem>-</asp:ListItem>
+                   <asp:ListItem>*</asp:ListItem>
+                   <asp:ListItem>/</asp:ListItem>
+       </asp:DropDownList> &nbsp;&nbsp;&nbsp;<asp:Literal ID="lt_rezultat"
+runat="server"></asp:Literal>
+       <br /><br />
+        <asp:Label ID="lb_rezultat" runat="server" Text="" /><br />
+       <br />
+           <asp:Button ID="Button4" runat="server" Text="Izračunaj
+rezultat" Height="23px" Width="93px" OnClick="Button4_Click" />
+       <br />
+      <asp:Button ID="Button3" runat="server" Text="Ispiši broj"
+Height="23px" Width="93px" OnClick="Button3_Click" />
+       <br />
+       <asp:Button ID="Button1" runat="server" Text="Klikni me"
+Height="23px" Width="93px" OnClick="Button1_Click" />
+       <asp:Button ID="Button2" runat="server" Text="Promijeni boju"
+Height="23px" Width="93px" OnClick="Button2_Click"  />
+   </div>
+   </form>
+</body>
+</html>
---
+++ Predavanje4/Default.aspx.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        p1.InnerHtml = "KLik na button..";
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Label1.Text = "Promijenjeno";
+        Label1.Visible = !Label1.Visible;
+        Label1.ForeColor = System.Drawing.Color.DarkSalmon;
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        p1.InnerHtml = "Uneseni broj je: " + tb_broj1.Text;
+    }
+
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        int broj1 = Int32.Parse(tb_broj1.Text);
+        int broj2 = Int32.Parse(tb_broj2.Text);
+
+        lb_rezultat.Text = "Zbroj je: " + (broj1 + broj2).ToString();
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender,
+ EventArgs e)
+    {
+        double rezultat = 0;
+        int broj1 = Int32.Parse(tb_broj1.Text);
+        int broj2 = Int32.Parse(tb_broj2.Text);
+        string operacija = DropDownList1.SelectedItem.ToString();
+        switch (operacija)
+        {
+            case "+":
+                rezultat = broj1 + broj2;
+                break;
+            case "-":
+                rezultat = broj1 - broj2;
+                break;
+            case "*":
+                rezultat = broj1 * broj2;
+                break;
+            case "/":
+                if (broj2 == 0)
+                {
+                    lt_rezultat.Text = "Greška, dijeljenje s 0!";
+                    return; //Nije baš lijep ali neka
+                }
+                else
+                {
+                    rezultat = broj1 / broj2;
+                }
+                break;
+        }
+        lt_rezultat.Text = "Rezltat je: " + rezultat.ToString();
+    }
+}
