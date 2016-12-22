0cc278c067a9dfefa3dd647f2160c57f535a8f25
vsite-prog <ivan.britvic@vsite.hr>
2016-12-22 19:09:04 +0100

    Entitity Framework, LINQ
    Entitity Framework, LINQ

---
+++ Predavanje 13/Default.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        Vlasnik:         <asp:DropDownList ID="ddl_vlasnici" runat="server" AutoPostBack="true"></asp:DropDownList>
+        <br />
+        <h4> Unesi novog psa</h4>
+        <asp:Label ID="Label1" runat="server" Text="UNesi ime psa: "></asp:Label>
+        <asp:TextBox ID="tb_ime" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <asp:LinkButton ID="LinkButton1" runat="server" OnClick="LinkButton1_Click">Spremi</asp:LinkButton>
+        <h4> Lista pasa</h4>
+        <p> &nbsp;</p>
+        <asp:GridView ID="GridView1" runat="server"></asp:GridView>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Predavanje 13/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    //Ovo je objekt koji predstavlja našu bazu
+    Entities db = new Entities();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            //Daj listu vlasnika
+            ddl_vlasnici.DataSource = db.Vlasnik.ToList<Vlasnik>();
+            //Pokaži tekst i value iz tablice
+            ddl_vlasnici.DataValueField = "id";
+            ddl_vlasnici.DataTextField = "ime";
+            //Refreshaj podatke
+            ddl_vlasnici.DataBind();
+        }
+
+        //Koji je trenutni vlasnik
+        int id = Int32.Parse(ddl_vlasnici.SelectedValue);
+        //Napuni GView pomoću LINQ-a
+        var psi = from pas in db.Pas //za svakog psa
+                  where pas.vlasnikId == id //vidi da li mu je vlasnik selektiran 
+                  select new { ID = pas.id, IME = pas.ime };
+        //Refresh
+        GridView1.DataSource = psi.ToList();
+        GridView1.DataBind();
+
+    }
+
+    protected void LinkButton1_Click(object sender, EventArgs e)
+    {
+        //Kreiraj novog psa u bazi
+        Pas pas = new Pas();
+        //Unesi mu podatke
+        pas.ime = tb_ime.Text;
+        pas.vlasnikId = Int32.Parse(ddl_vlasnici.SelectedValue);
+        //Dodaj ga u listu
+        db.Pas.Add(pas);
+        //Spremi u bazu
+        db.SaveChanges();
+    }
+}
