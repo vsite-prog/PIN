c107025ba507bfb5922141ac74e82c0f02300601
vsite-prog <ivan.britvic@vsite.hr>
2017-12-04 17:03:53 +0100

    ASP.NET Core osnove
    ASP.NET Core osnove

---
+++ CoreAppEmpty/CoreAppEmpty/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreAppEmpty.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            //return Ok("Haj, haj, idemoooo...");
+            return View();
+        }
+
+        [Route("/Druga/")]
+        public IActionResult Druga()
+        {
+            return View();
+        }
+    }
+}
