4f53f5baebae4d8c5afeff54f26de6b9df5b6b69
vsite-prog <ivan.britvic@vsite.hr>
2018-10-25 19:30:00 +0200

    Osnovne web kontrole
    Osnovne web kontrole

---
+++ Predavanje 4/Predavanje 4/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div id="div1">
+            <asp:Label ID="Label2" AssociatedControlID="tb_unos" runat="server" Text="Unesi text!"></asp:Label>
+            <asp:TextBox ID="tb_unos" runat="server"></asp:TextBox>
+            <asp:Literal ID="Literal1" runat="server" Text="<strong>Hi</strong>"></asp:Literal>
+            <asp:Button ID="B2" runat="server" Text="Unesi" OnClick="B2_Click" />
+            <br />
+            <p id="par1" runat="server">POčetni tekst</p>
+            <asp:Label ID="Label1" runat="server" Text=""></asp:Label><br />
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+            <h4>Kalkulator</h4>
+            <p>
+            <asp:TextBox ID="tb_a" runat="server"></asp:TextBox>
+            &nbsp;&nbsp;
+                <asp:DropDownList  AutoPostBack="true" ID ="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+                    <asp:ListItem>+</asp:ListItem>
+                    <asp:ListItem>-</asp:ListItem>
+                    <asp:ListItem>*</asp:ListItem>
+                    <asp:ListItem>/</asp:ListItem>
+                </asp:DropDownList>
+&nbsp;&nbsp;
+            <asp:TextBox ID="tb_b" runat="server"></asp:TextBox>
+            &nbsp;=
+            <asp:TextBox ID="tb_rez" runat="server" ReadOnly="True"></asp:TextBox>
+            </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Predavanje 4/Predavanje 4/Default.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page { 
+
+    // Globalna varijabla
+    int i = 0;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        //Koje je vrijeme  otvarnja stranice od strane {
+        if (!IsPostBack)
+        {
+          Label1.Text = DateTime.Now.ToLongTimeString();
+            //Promijeni izgled
+            Label1.ForeColor = System.Drawing.Color.Red;
+            Label1.BackColor = System.Drawing.Color.LightGray;
+            //Dosta je jedan put ovo staviti
+            tb_rez.Enabled = false;
+        }
+
+    }
+
+
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        i++;
+        Label1.Text += "<br> Postback broj: " + i;
+       par1.InnerHtml = "Promijenio!";
+
+    }
+
+    protected void B2_Click(object sender, EventArgs e)
+    {
+        par1.InnerHtml = "Unijeli ste"  + tb_unos.Text;
+        Literal1.Text += "<br>";
+        //A što ako želimo ovakav tekst? 
+        //Literal1.Text += "<div> fsdfsfsa";  < = &lt; 
+        Literal1.Text += Server.HtmlEncode("<div> fsdfsfsa");
+    }
+
+    protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        double a, b, rez = 0;
+        //Bilo bi dobro ovdje hvatati exception-e
+        a = Double.Parse(tb_a.Text);
+        b = Double.Parse(tb_b.Text);
+        //Pročitaj iz DDL-a koja operacija
+        string operacija = DropDownList1.SelectedValue;
+        switch (operacija)
+        {
+            case "+":
+                rez = a + b;
+                break;
+            case "-":
+                rez = a - b;
+                break;
+            case "*":
+                rez = a * b;
+                break;
+            case "/":
+                rez = a / b;
+                break;
+        }
+        tb_rez.Text = rez.ToString();
+    }
+}
