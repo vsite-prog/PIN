55eab034b17b89aef07252cfd161d0f252b82c22
vsite-prog <ivan.britvic@vsite.hr>
2018-11-28 15:54:32 +0100

    ASP.NET Core osnove
    ASP.NET Core osnove

---
+++ EmptyCoreProject/EmptyCoreProject/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmptyCoreProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Ovdje u atributu kažemo da spoji URL sa kontrolerom (MVC servis)
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return Ok("Pozdrav iz kontrolera!");
+            //return View();
+        }
+
+        public IActionResult Show()
+        {
+            return View();
+        }
+    }
+}
