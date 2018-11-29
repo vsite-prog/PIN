f4e66da2df448c449ce7d684b5a9b10af5fdc152
vsite-prog <ivan.britvic@vsite.hr>
2018-11-29 18:23:10 +0100

    MVC ASP.NET Core osnovna aplikacija
    MVC ASP.NET Core osnovna aplikacija

---
+++ MVCCoreApp/MVCCoreApp/Controllers/PinController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCCoreApp.Models;
+
+namespace MVCCoreApp.Controllers
+{
+    public class PinController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Imamo objekt koji služi za povezivanje 
+            ViewBag.Pin = "Pin je super, učim ga svaki dan...";
+            ViewBag.Title = "PIN";
+            return View();
+        }
+
+        public IActionResult Ajmo()
+        {
+            // Možemo naizmjence koristiti ViewData i ViewBag
+            ViewData["ajme"] = "Idemo vježbati za kolokvij...";
+            return View();
+        }
+
+    }
+}
