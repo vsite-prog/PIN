e33de562e732423535b49495fd95e29824bdf68e
vsite-prog <ivan.britvic@vsite.hr>
2019-12-16 10:32:55 +0100

    Osnovna ASP.NET Core aplikacija
    Osnovna ASP.NET Core aplikacija

---
+++ EmptyCoreApp/EmptyCoreApp/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmptyCoreApp.Controllers
+{
+    public class DefaultController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/drugi")]
+        public IActionResult Drugi()
+        {
+            // Nemamo View
+            return Ok("POzdrav iz kontrolera!");
+        }
+    }
+}
