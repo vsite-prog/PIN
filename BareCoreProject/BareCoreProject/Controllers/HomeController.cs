3290b3a9879e4a0967d2464da2efce068a1a6b76
vsite-prog <ivan.britvic@vsite.hr>
2020-11-27 18:31:06 +0100

    ASP.NET Core jednostavna aplikacija
    ASP.NET Core jednostavna aplikacija

---
+++ BareCoreProject/BareCoreProject/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BareCoreProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            return Ok("Pozdrav iz kontrolera!");
+            // return View();
+        }
+    }
+}
